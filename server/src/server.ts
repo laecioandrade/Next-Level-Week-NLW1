a496b26e8591c2c9e094520e889e7558ab6f54e9
laecioandrade <laecio_a.rodrigues@outlook.com>
2020-06-03 00:01:15 -0300

    Adding backend functionality: database connection, registration, listing and data search ...

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,14 @@
 import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    console.log('Hello World');
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
-    //response.json([
-        //'Lucas'
-    //]);
-});
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
